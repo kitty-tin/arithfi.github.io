a53d75a5039b8bd65a191ae70ab62a74e9cf388e
tunogya <tunogya@qq.com>
2023-09-23 20:51:52 +0800

    remove "edit this page"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,15 +42,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ArithFi/arithfi.github.io/edit/master/',
+          // editUrl:
+          //   'https://github.com/ArithFi/arithfi.github.io/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ArithFi/arithfi.github.io/edit/master/',
+          // editUrl:
+          //   'https://github.com/ArithFi/arithfi.github.io/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
