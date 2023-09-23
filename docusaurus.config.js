3cb294216a88f19792903c8b0426594f7166e905
tunogya <tunogya@qq.com>
2023-09-23 20:42:43 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/arithfi/arithfi.github.io/docs/',
+            'https://github.com/ArithFi/arithfi.github.io/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/arithfi/arithfi.github.io/blog/',
+            'https://github.com/ArithFi/arithfi.github.io/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
