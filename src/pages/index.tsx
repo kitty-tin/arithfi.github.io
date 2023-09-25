bc989d891e680ed2ae3a12a895cf256fcc42a3ca
tunogya <tunogya@qq.com>
2023-09-25 10:17:39 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home(): JSX.Element {
 
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Docs">
       <main>
       </main>
     </Layout>
